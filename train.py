6db88780e1f8611bfabf3f901b33a2be1c00e89e
Adithya Ganesh <adithya.cganesh@gmail.com>
2021-05-09 13:51:35 -0500

    update

--- train.py
+++ train.py
@@ -1,27 +1,106 @@
+import os
+import argparse
+import multiprocessing
+from pathlib import Path
+from PIL import Image
+
 import torch
+from torchvision import models, transforms
+from torch.utils.data import DataLoader, Dataset
+
 from byol_pytorch import BYOL
-from torchvision import models
+import pytorch_lightning as pl
 
-resnet = models.resnet50(pretrained=False)
+model = models.resnet50(pretrained=True)
 
-learner = BYOL(
+BATCH_SIZE = 32
+EPOCHS     = 1000
+LR         = 3e-4
+NUM_GPUS   = 2
+IMAGE_SIZE = 256
+IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
+NUM_WORKERS = multiprocessing.cpu_count()
+
+# pytorch lightning module
+
+class SelfSupervisedLearner(pl.LightningModule):
+    def __init__(self, net, **kwargs):
+        super().__init__()
+        self.learner = BYOL(net, **kwargs)
+
+    def forward(self, images):
+        return self.learner(images)
+
+    def training_step(self, images, _):
+        loss = self.forward(images)
+        return {'loss': loss}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=LR)
+
+    def on_before_zero_grad(self, _):
+        if self.learner.use_momentum:
+            self.learner.update_moving_average()
+
+# images dataset
+
+def expand_greyscale(t):
+    return t.expand(3, -1, -1)
+
+class ImagesDataset(Dataset):
+    def __init__(self, folder, image_size):
+        super().__init__()
+        self.folder = folder
+        self.paths = []
+
+        for path in Path(f'{folder}').glob('**/*'):
+            _, ext = os.path.splitext(path)
+            if ext.lower() in IMAGE_EXTS:
+                self.paths.append(path)
+
+        print(f'{len(self.paths)} images found')
+
+        self.transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.CenterCrop(image_size),
+            transforms.ToTensor(),
+            transforms.Lambda(expand_greyscale)
+        ])
+
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, index):
+        path = self.paths[index]
+        img = Image.open(path)
+        img = img.convert('RGB')
+        return self.transform(img)
+
+# === 
+
+import torchvision
+ds = torchvision.datasets.STL10("./STL10", download=True)
+
+ds = ImagesDataset("./unlabeled_images", 96)
+
+train_loader = DataLoader(ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
+
+NUM_GPUS=1
+resnet = models.resnet18(pretrained=False)
+model = SelfSupervisedLearner(
     resnet,
-    image_size = 256,
-    hidden_layer = 'avgpool'
+    image_size = IMAGE_SIZE,
+    hidden_layer = 'avgpool',
+    projection_size = 256,
+    projection_hidden_size = 4096,
+    moving_average_decay = 0.99
 )
 
-opt = torch.optim.Adam(learner.parameters(), lr=3e-4)
+trainer = pl.Trainer(
+    gpus = NUM_GPUS,
+    max_epochs = 1,
+    accumulate_grad_batches = 1,
+    sync_batchnorm = True
+)
 
-def sample_unlabelled_images():
-    return torch.randn(20, 3, 256, 256)
-
-for _ in range(100):
-    images = sample_unlabelled_images()
-    loss = learner(images)
-    opt.zero_grad()
-    loss.backward()
-    opt.step()
-    learner.update_moving_average() # update moving average of target encoder
-
-# save your improved network
-torch.save(resnet.state_dict(), './improved-net.pt')
+trainer.fit(model, train_loader)
