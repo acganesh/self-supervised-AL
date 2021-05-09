e01dc4de2313029cb81afaad0db5d70038856453
Adithya Ganesh <adithya.cganesh@gmail.com>
2021-05-09 15:37:16 -0500

    update

---
+++ models/model.py
@@ -0,0 +1,23 @@
+import pytorch_lightning as pl
+import torch
+
+from byol_pytorch import BYOL
+
+class SelfSupervisedLearner(pl.LightningModule):
+    def __init__(self, net, **kwargs):
+        super().__init__()
+        self.learner = BYOL(net, **kwargs)
+
+    def forward(self, images):
+        return self.learner(images)
+
+    def training_step(self, images, _):
+        loss = self.forward(images)
+        return {'loss': loss}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=LR)
+
+    def on_before_zero_grad(self, _):
+        if self.learner.use_momentum:
+            self.learner.update_moving_average()
