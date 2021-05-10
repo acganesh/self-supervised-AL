b1e1d9aea6cd1e22adb14e272e3a5f0a0a2cf9f0
Adithya Ganesh <adithya.cganesh@gmail.com>
2021-05-10 12:43:38 -0500

    update gitig

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import sklearn
 import torch
 import torchvision
 from torch.utils.data import DataLoader
+import sklearn
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 
@@ -54,9 +55,9 @@ def main(argv):
         print("Loaded checkpoint from ", argv[2])
 
         #TODO: for some reason labels don't exist in my wget data 
-        #ds = ImagesDataset("./data/test_images", IMAGE_SIZE, train=False)
+        #ds = ImagesDataset("./dataset/test_images", IMAGE_SIZE, train=False)
         data_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-        train_dataset = torchvision.datasets.STL10('./data/train_split', split='train', download=False,
+        train_dataset = torchvision.datasets.STL10('./dataset/train_split', split='train', download=False,
                                transform=data_transforms)
         train_loader = DataLoader(train_dataset, batch_size=5000, num_workers=NUM_WORKERS, shuffle=False)
 
@@ -64,7 +65,7 @@ def main(argv):
         train_imgs, train_labels = next(iter(train_loader))
         print("Train loading done")
 
-        test_dataset = torchvision.datasets.STL10('./data/test_split', split='test', download=False, transform=data_transforms)
+        test_dataset = torchvision.datasets.STL10('./dataset/test_split', split='test', download=False, transform=data_transforms)
         test_loader = DataLoader(test_dataset, batch_size=8000, num_workers=NUM_WORKERS, shuffle=False)
         test_imgs, test_labels = next(iter(test_loader))
         print("Test loading done")
@@ -76,7 +77,14 @@ def main(argv):
 
         train_imgs = torch.flatten(train_imgs, start_dim=1)
         test_imgs = torch.flatten(test_imgs, start_dim=1)
-        pca = PCA(n_components=256)
+
+        scaler = sklearn.preprocessing.StandardScaler()
+        scaler.fit(train_imgs)
+        train_imgs = scaler.transform(train_imgs).astype(np.float32)
+        test_imgs = scaler.transform(test_imgs).astype(np.float32)
+
+
+        pca = PCA(n_components=512)
         train_imgs_pca = pca.fit_transform(train_imgs)
         test_imgs_pca = pca.transform(test_imgs)
 
@@ -88,10 +96,10 @@ def main(argv):
         baseline_acc = sklearn.metrics.accuracy_score(test_labels, baseline_classes)
 
         lr_byol = LogisticRegression(max_iter=100000)
-        lr_byol.fit(train_projs.detach().numpy(), train_labels)
+        lr_byol.fit(train_embeddings.detach().numpy(), train_labels)
 
-        byol_preds = lr_byol.predict_proba(test_projs.detach().numpy())
-        byol_classes = lr_byol.predict(test_projs.detach().numpy())
+        byol_preds = lr_byol.predict_proba(test_embeddings.detach().numpy())
+        byol_classes = lr_byol.predict(test_embeddings.detach().numpy())
         byol_acc = sklearn.metrics.accuracy_score(test_labels, byol_classes)
 
         import pdb; pdb.set_trace()
