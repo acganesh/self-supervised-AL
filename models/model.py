9701c4a7cb85c143a9bfad7b9513e093d98b3a4f
Adithya Ganesh <adithya.cganesh@gmail.com>
2021-05-09 20:00:22 -0500

    update

--- models/model.py
+++ models/model.py
@@ -4,9 +4,10 @@ import torch
 from byol_pytorch import BYOL
 
 class SelfSupervisedLearner(pl.LightningModule):
-    def __init__(self, net, **kwargs):
+    def __init__(self, net, lr, **kwargs):
         super().__init__()
         self.learner = BYOL(net, **kwargs)
+        self.lr = lr
 
     def forward(self, images):
         return self.learner(images)
@@ -16,8 +17,16 @@ class SelfSupervisedLearner(pl.LightningModule):
         return {'loss': loss}
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=LR)
+        return torch.optim.Adam(self.parameters(), lr=self.lr)
 
     def on_before_zero_grad(self, _):
         if self.learner.use_momentum:
             self.learner.update_moving_average()
+
+class LogisticRegression(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(LogisticRegression, self).__init__()
+        self.linear = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        return self.linear(x)
--- train.py
+++ train.py
@@ -2,14 +2,17 @@ import multiprocessing
 import sys
 
 import pytorch_lightning as pl
+import sklearn
 import torch
 import torchvision
 from torch.utils.data import DataLoader
+from sklearn.linear_model import LogisticRegression
+from sklearn.decomposition import PCA
 
 from models.model import SelfSupervisedLearner
 from data.dataloaders import ImagesDataset
 
-BATCH_SIZE = 32
+BATCH_SIZE = 256
 EPOCHS     = 1000
 LR         = 3e-4
 IMAGE_SIZE = 96 # Change this depending on dataset
@@ -26,7 +29,8 @@ def main(argv):
         hidden_layer = 'avgpool',
         projection_size = 256,
         projection_hidden_size = 4096,
-        moving_average_decay = 0.99
+        moving_average_decay = 0.99,
+        lr = LR
     )
 
     if (argv[1] == "--train"):
@@ -36,12 +40,14 @@ def main(argv):
 
         trainer = pl.Trainer(
             gpus = NUM_GPUS,
-            max_epochs = 1,
+            max_epochs = 10,
             accumulate_grad_batches = 1,
             sync_batchnorm = True
         )
 
         trainer.fit(model, train_loader)
+
+        import pdb; pdb.set_trace()
     elif argv[1] == "--load":
         torch.load(argv[2])
         print("Loaded checkpoint from ", argv[2])
@@ -49,11 +55,43 @@ def main(argv):
         #TODO: for some reason labels don't exist in my wget data 
         #ds = ImagesDataset("./data/test_images", IMAGE_SIZE, train=False)
         data_transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-        test_dataset = torchvision.datasets.STL10('./data/test_split', split='test', download=True,
+        train_dataset = torchvision.datasets.STL10('./data/train_split', split='train', download=False,
                                transform=data_transforms)
-        test_loader = DataLoader(ds, batch_size=8000, num_workers=NUM_WORKERS, shuffle=False)
+        train_loader = DataLoader(train_dataset, batch_size=5000, num_workers=NUM_WORKERS, shuffle=False)
 
-        imgs = next(iter(test_loader))
+
+        train_imgs, train_labels = next(iter(train_loader))
+        print("Train loading done")
+
+        test_dataset = torchvision.datasets.STL10('./data/test_split', split='test', download=False, transform=data_transforms)
+        test_loader = DataLoader(test_dataset, batch_size=8000, num_workers=NUM_WORKERS, shuffle=False)
+        test_imgs, test_labels = next(iter(test_loader))
+        print("Test loading done")
+
+        train_projs, train_embeddings = model.learner.forward(train_imgs, return_embedding=True)
+        test_projs, test_embeddings = model.learner.forward(test_imgs, return_embedding=True)
+
+        print("got embeddings")
+
+        train_imgs = torch.flatten(train_imgs, start_dim=1)
+        test_imgs = torch.flatten(test_imgs, start_dim=1)
+        pca = PCA(n_components=256)
+        train_imgs_pca = pca.fit_transform(train_imgs)
+        test_imgs_pca = pca.transform(test_imgs)
+
+        lr_baseline = LogisticRegression(max_iter=100000)
+        baseline_preds = lr_baseline.fit(train_imgs_pca, train_labels)
+
+        baseline_preds = lr_baseline.predict_proba(test_imgs_pca)
+        baseline_classes = lr_baseline.predict(test_imgs_pca)
+        baseline_acc = sklearn.metrics.accuracy_score(test_labels, baseline_classes)
+
+        lr_byol = LogisticRegression(max_iter=100000)
+        lr_byol.fit(train_projs.detach().numpy(), train_labels)
+
+        byol_preds = lr_byol.predict_proba(test_projs.detach().numpy())
+        byol_classes = lr_byol.predict(test_projs.detach().numpy())
+        byol_acc = sklearn.metrics.accuracy_score(test_labels, byol_classes)
 
         import pdb; pdb.set_trace()
 
