141940e65d1ac4c26fbe661b8b06eb68dfa6d7e1
Adithya Ganesh <adithya.cganesh@gmail.com>
2021-06-01 01:07:00 -0500

    cifar scripts

---
+++ cifar_scripts/cifar_test.py
@@ -0,0 +1,35 @@
+import torchvision
+import numpy as np
+import os
+from collections import defaultdict
+from tqdm import tqdm
+
+num_classes = 10
+
+ROOT_DIR = './dataset/'
+save_dir = os.path.join(ROOT_DIR, 'biased_cifar10/test/')
+cifar_dir = os.path.join(ROOT_DIR, 'cifar10_raw')
+
+cifar10 = torchvision.datasets.CIFAR10(
+    root=cifar_dir,
+    train=False,
+    download=True)
+
+it = iter(cifar10)
+num_images = 0
+indices_to_use = {}
+cur_class_index = defaultdict(int)
+channel_sum = np.zeros(3)
+channel_sum_squared = np.zeros(3)
+num_pixels = 0
+for class_index in range(num_classes):
+    num_to_sample = (class_index + 1) * 100
+    index_to_sample = np.linspace(0, 999, num=num_to_sample, dtype=int)
+    indices_to_use[class_index] = set(index_to_sample)
+for i, image in tqdm(enumerate(it)):
+    img, label = image
+    cur_index = cur_class_index[label]
+    if cur_index in indices_to_use[label]:
+        save_location = save_dir + f"image_{i}_label_{label}.png"
+        img.save(save_location, format="png")
+    cur_class_index[label] = cur_class_index[label] + 1
---
+++ config.py
@@ -0,0 +1,19 @@
+config_local = {
+    # Data
+    'STL10_TRAIN': './dataset/stl10/train_split'
+    'STL10_TEST': './dataset/stl10/test_split',
+    'SVHN_EXTRA': './dataset/svhn/extra',
+    'SVHN_TEST': './dataset/svhn/test',
+    'CIFAR10_RAW': './dataset/cifar10_raw',
+    'BIASED_CIFAR10_TRAIN': './dataset/biased_cifar10/train',
+    'BIASED_CIFAR10_TEST': './dataset_biased_cifar10/test'
+
+    # Checkpoints
+    'STL10_WEIGHTS': './ckpt/learner_0510_v100.pt',
+    'SVHN_WEIGHTS': None, # populate
+    'CIFAR10_WEIGHTS': None,
+}
+
+# TODO: fill in
+config_cluster = {
+}
